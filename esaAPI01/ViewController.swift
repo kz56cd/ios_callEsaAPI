66e0a81a56ce0574c04680680e1274047953bee4
kz56cd <kz56cd@gmail.com>
2015-07-26 11:59:21 +0900

    init commit

---
+++ esaAPI01/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  esaAPI01
+//
+//  Created by 佐野正和 on 2015/07/26.
+//  Copyright (c) 2015年 佐野正和. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    
+
+
+}
+
